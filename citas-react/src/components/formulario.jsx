dd8dbe9814be941aa64339611c17dc6bb5880833
kmendez <62451263+Kajoha@users.noreply.github.com>
2024-03-07 16:02:51 -0600

    Tres
    Hook formulario - primera parte

--- citas-react/src/components/formulario.jsx
+++ citas-react/src/components/formulario.jsx
@@ -1,10 +1,16 @@
 /* rfce es un acceso rapido para crear el componente con function
 rafc usa const con un arrow function
 */
-
+import { useState } from 'react' /* Para poder usar los hooks tengo que hacer este impor*/
 
 
 const Formulario = () => {
+    /* Los hooks deben ir antes del return pero tambien deben ir antes que otras funciones y no pueden 
+    ir fuera del componente*/
+    const [nombre, setNombre] = useState('');
+    /* setNombre('Hook') - setnombre es la función modificadora del estado */
+
+
     return (
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
@@ -16,19 +22,36 @@ const Formulario = () => {
             <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre mascota</label>
-                    <input id="mascota" type="text" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Nombre de la mascota" />
+                    <input id="mascota"
+                        type="text"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        placeholder="Nombre de la mascota"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                    />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">Nombre propietario</label>
-                    <input id="propietario" type="text" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Nombre del propietario" />
+                    <input id="propietario"
+                        type="text"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        placeholder="Nombre del propietario"
+                    />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="email" className="block text-gray-700 uppercase font-bold">Email</label>
-                    <input id="email" type="email" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Email contacto propietario" />
+                    <input id="email"
+                        type="email"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        placeholder="Email contacto propietario"
+                    />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">Fecha de alta</label>
-                    <input id="alta" type="date" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                    <input id="alta"
+                        type="date"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Sintomas</label>
