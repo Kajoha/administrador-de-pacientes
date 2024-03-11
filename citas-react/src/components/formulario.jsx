180a4a2df1163015b5e6033304be1ce9dbbc32e4
kmendez <62451263+Kajoha@users.noreply.github.com>
2024-03-11 12:12:32 -0600

    Cuatro
    Hook Formulario - segunda parte

--- citas-react/src/components/formulario.jsx
+++ citas-react/src/components/formulario.jsx
@@ -7,9 +7,25 @@ import { useState } from 'react' /* Para poder usar los hooks tengo que hacer es
 const Formulario = () => {
     /* Los hooks deben ir antes del return pero tambien deben ir antes que otras funciones y no pueden 
     ir fuera del componente*/
-    const [nombre, setNombre] = useState('');
     /* setNombre('Hook') - setnombre es la función modificadora del estado */
+    const [nombre, setNombre] = useState('');
+    const [propietario, setPropietario] = useState('');
+    const [email, setEmail] = useState('');
+    const [fecha, setFecha] = useState('');
+    const [sintomas, setSintomas] = useState('');
 
+    const [error, setError] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //Validación del formulario
+
+        if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+            console.log('Hay al menos un campo vacio')
+            setError(true)
+            return
+        } setError(false)
+    }
 
     return (
         <div className="md:w-1/2 lg:w-2/5">
@@ -19,7 +35,11 @@ const Formulario = () => {
                 <span className="text-indigo-600 font-bold">Administralos</span>
             </p>
 
-            <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <form
+                onSubmit={handleSubmit}
+                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mx-5">
+                {error && <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md '><p>Todos los campos son obligatorios</p></div>}
+
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre mascota</label>
                     <input id="mascota"
@@ -36,6 +56,8 @@ const Formulario = () => {
                         type="text"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         placeholder="Nombre del propietario"
+                        value={propietario}
+                        onChange={(e) => setPropietario(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -44,6 +66,8 @@ const Formulario = () => {
                         type="email"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         placeholder="Email contacto propietario"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -51,11 +75,17 @@ const Formulario = () => {
                     <input id="alta"
                         type="date"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={fecha}
+                        onChange={(e) => setFecha(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Sintomas</label>
-                    <textarea id="sintomas" className="border-2 w-full p-27 mt-2 placeholder-gray-400 rounded-md" placeholder="Describe los sintomas" />
+                    <textarea
+                        id="sintomas"
+                        className="border-2 w-full p-27 mt-2 placeholder-gray-400 rounded-md" placeholder="Describe los sintomas"
+                        value={sintomas}
+                        onChange={(e) => setSintomas(e.target.value)} />
                 </div>
                 <input type="submit" className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors" value="Agregar paciente" />
             </form>
