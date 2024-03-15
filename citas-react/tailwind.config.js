d3f83b3319c4ec4574c315cdff0d0562b9dde6d4
kmendez <62451263+Kajoha@users.noreply.github.com>
2024-03-15 09:52:27 -0600

    Config final

--- citas-react/tailwind.config.js
+++ citas-react/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.jsx'], /* El diagonal act act significa que va a buscar 
+  content: ['index.html', './src/**/*.jsx'], /* El diagonal act act significa que va a buscar 
   en todas las carpetas los archivos y el diagonal act.jsx significa que busque los archivos que terminan en 
   .jxs asi que va a aplicar la configuración en todos esos documentos*/
   theme: {
